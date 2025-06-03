ef0f2e8da5c22d2bd9dee053e37a4ca79218ef95
REZA DYSTA SATRIA <87264688+dystaSatria@users.noreply.github.com>
2025-06-03 04:45:38 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,7 @@ st.markdown("""
         overflow: hidden;
         white-space: nowrap;
         border-right: 3px solid #00ff00;
-        animation: typing 4s steps(50) infinite, blink-caret 0.75s step-end infinite;
+        animation: typing 9s steps(50) infinite, blink-caret 0.75s step-end infinite;
         width: 100%;
         max-width: fit-content;
         margin-left: auto;
